e3f032171ab711dfb936066be275b2bd7aec10cc
Roman <romagilda@yandex.ru>
2025-09-01 12:45:57 +0300

    build

--- src/components/ContentAddress/SubComponents/Comment/Comment.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import style from "./Comment.module.scss";
-
-export const Comment = ({ data }) => {
-  if (!data.comment) return null;
-  return <p className={style.info}>{data.comment}</p>;
-};
--- src/components/ContentAddress/SubComponents/Head/Head.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './Head.module.scss'
-
-export const Head = ({objectAddress}) => {
-  return (
-    <h2 className={style.address_head}>{objectAddress.address}</h2>
-  )
-}
