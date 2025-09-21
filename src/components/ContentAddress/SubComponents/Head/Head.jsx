69b4be20c419511307ab1f24068861175ed7f6b1
Roman <romagilda@yandex.ru>
2025-09-21 12:29:27 +0300

    add copy To Clipboard

--- src/components/ContentAddress/SubComponents/Head/Head.jsx
+++ src/components/ContentAddress/SubComponents/Head/Head.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import style from './Head.module.scss'
+import { Layers } from "lucide-react"
+import toast, { Toaster } from "react-hot-toast";
+
 
 export const Head = ({objectAddress}) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(objectAddress.address)
+      .then(() => toast.success("Адрес скопирован"))
+      .catch(() => toast.error("Ошибка копирования"));
+  }
+
   return (
-    <h2 className={style.address_head}>{objectAddress.address}</h2>
+    <div className={style.headWrapper}>
+      <h2 className={style.address_head}>{objectAddress.address}</h2>
+      <button className={style.copyBtn} onClick={copyToClipboard} title="Скопировать адрес">
+        <Layers size={18} />
+      </button>
+      <Toaster position="top-right" />
+    </div>
   )
 }
+
